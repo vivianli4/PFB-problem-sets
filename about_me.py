60584c244b14069bc8815dbf753dd5b858baf066
Vivian <yli03@rockefeller.edu>
2025-10-14 12:11:59 -0400

    about me section

--- about_me.py
+++ about_me.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+import sys
 
-print ('My name is: Vivian')
+# print ('My name is: Vivian')
+# print ('My favorite color: blue')
+# activity = 'coding'
+# #print ('my fave activity', activity)
+# message= f"my fave activity: {activity}"
+#print (message)
+my_name= sys.argv[1]
+fave_color = sys.argv[2]
+print('my name is', my_name, 'my favorite color', fave_color)
