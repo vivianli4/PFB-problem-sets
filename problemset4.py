61b9dc00cccfe107ea6437cf7dc6d93c9e3bd113
Vivian <yli03@rockefeller.edu>
2025-10-15 20:25:23 -0400

    finished problem set

--- problemset4.py
+++ problemset4.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #Oct 15, 2025 Vivian Li 
 
+import sys
+
 #question 2 list manipulation
 # taxa_string = 'sapiens : eruectus : neaderthalensis'
 # #print(taxa_string)
@@ -16,10 +18,72 @@
 # print(sorted(len(name) for name in taxa_list_iterator))
 
 #question 4: write a while loop
-count =0
-sum=0
-while count <= 100 : 
-    print(count)
-    sum=sum+count
-    count+=1
-print(sum)
+# count =0
+# sum=0
+# while count <= 100 : 
+#     print(count)
+#     sum=sum+count
+#     count+=1
+# print(sum)
+
+#question 5 find the product of 10!
+# count=0
+# product = 10
+# while count <10:
+#     product = product * (count+1)
+#     count=count+1
+# print(product)
+
+# #question6 for loop through list
+# even_total=0
+# odd_total=0
+# myList=[101,2,15,22,95,33,2,27,72,15,52]
+
+# for num in myList: 
+#     if num%2 ==0: 
+#         even_total=even_total+num
+#         print(num)
+#     else:
+#         print(num)
+#         odd_total=odd_total+num
+# print(f'Sum of even numbers: {even_total}\nSum of odd numbers: {odd_total}')
+
+#question8 loops and ranges
+# nums=range(1,101)
+# for i in nums:
+#     print(i)
+
+#question 9 
+
+# num_list= [range(100)]
+# num_list2= [range(1, 101)]
+
+# #print(num_list)
+# print(num_list2)
+
+# #question 10 user inputs
+# min=int(sys.argv[1])
+# max=int(sys.argv[2]) +1
+# ranges=range(min,max)
+# for i in ranges:
+#     print(i)
+
+# #question11 do the same as 10 but using line comprehesion
+# min=int(sys.argv[1])
+# max=int(sys.argv[2]) +1
+# ranges=[i for i in range(min, max)]
+# print(ranges)
+
+#question 12 add a condition that only odd numbers are added to list
+# min=int(sys.argv[1])
+# max=int(sys.argv[2]) +1
+# ranges=[i for i in range(min, max)]
+# oddNumbers=[num for num in ranges if num%2==1]
+# print(oddNumbers)
+
+#question13
+sequences= ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
+for i in sequences:
+    print(f'{sequences.index(i)}\t{len(i)}\t{i} \n')
+
+
